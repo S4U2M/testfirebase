8d779e0bfe8cfd587006517225d7ccb0c6673684
송규진 <songu9610@gmail.com>
2023-10-16 17:14:15 +0900

    test

--- app/src/main/java/com/firebase/testlogin/data/repo/room/RoomRepo.kt
+++ app/src/main/java/com/firebase/testlogin/data/repo/room/RoomRepo.kt
@@ -1,8 +1,9 @@
 package com.firebase.testlogin.data.repo.room
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import com.firebase.testlogin.data.model.local.TestEntity
-
+@Dao
 interface RoomRepo {
 
     fun getAllDataFromRoom():LiveData<List<TestEntity>>
